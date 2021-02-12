94d7da406c7403fe123763b8f0997c2d4fde0057
Justin Katerberg <j.katerberg10@gmail.com>
2021-02-12 13:45:52 -0500

    lab04 extra challenge

--- lab04/src/register/Controller.java
+++ lab04/src/register/Controller.java
@@ -1,10 +1,13 @@
 package register;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.util.StringConverter;
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Controller {
     public DatePicker birthField;
@@ -14,13 +17,49 @@ public class Controller {
     public TextField emailField;
     public TextField phoneField;
     public Button submitButton;
+    public Label emailErrorLabel;
+    private final EmailValidator emailValidator = EmailValidator.getInstance();
+
+    private DateTimeFormatter dateTimeFormatter;
+
+    @FXML
+    public void initialize(){
+        final String datePattern = "yyyy-MM-dd";
+        dateTimeFormatter = DateTimeFormatter.ofPattern(datePattern);
+        birthField.setConverter(new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                String finalDate = null;
+                if(date!=null)
+                    finalDate = dateTimeFormatter.format(date);
+                return finalDate;
+            }
+
+            @Override
+            public LocalDate fromString(String s) {
+                LocalDate date = null;
+                if (s != null)
+                    date = LocalDate.parse(s, dateTimeFormatter);
+                return date;
+            }
+        });
+
+    }
 
     public void submitButtonClicked(ActionEvent actionEvent) {
-        System.out.println("Username: " + usernameField.getText());
-        System.out.println("Password: " + passwordField.getText());
-        System.out.println("Full Name: " + fullNameField.getText());
-        System.out.println("E-mail: " + emailField.getText());
-        System.out.println("Phone: " + phoneField.getText());
-        System.out.println("Birthday: " + birthField.getValue());
+        //Clear any previous errors
+        emailErrorLabel.setText("");
+        //check for valid inputs
+        if(emailValidator.isValid(emailField.getText())) {
+            System.out.println("Username: " + usernameField.getText());
+            System.out.println("Password: " + passwordField.getText());
+            System.out.println("Full Name: " + fullNameField.getText());
+            System.out.println("E-mail: " + emailField.getText());
+            System.out.println("Phone: " + phoneField.getText());
+            System.out.println("Birthday: " + birthField.getEditor().getText());
+        }else{
+            emailField.selectAll();
+            emailErrorLabel.setText("Invalid Email Address");
+        }
     }
 }
