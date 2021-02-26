61d681ab1b1ffeced2419f5eb236fad25206b831
Justin Katerberg <j.katerberg10@gmail.com>
2021-02-26 13:07:07 -0500

    lab05 extra

--- lab05/src/gradeCalculator/Controller.java
+++ lab05/src/gradeCalculator/Controller.java
@@ -2,15 +2,21 @@ package gradeCalculator;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.fxml.Initializable;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    public TextField sidInput;
+    public TextField assignmentsInput;
+    public TextField midtermInput;
+    public TextField examInput;
+    public Button addButton;
+    public Label errorLabel;
     @FXML private TableView<StudentRecord> table;
     @FXML private TableColumn<StudentRecord, String> sidColumn;
     @FXML private TableColumn<StudentRecord, Float> assignmentColumn;
@@ -19,6 +25,8 @@ public class Controller implements Initializable {
     @FXML private TableColumn<StudentRecord, Float> finalColumn;
     @FXML private TableColumn<StudentRecord, Character> letterColumn;
 
+
+    private final ObservableList<StudentRecord> studentList = DataSource.getAllMarks();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -34,5 +42,31 @@ public class Controller implements Initializable {
         table.setItems(studentList);
     }
 
-    private final ObservableList<StudentRecord> studentList = DataSource.getAllMarks();
+
+
+    public void addButtonHandler(javafx.event.ActionEvent actionEvent) {
+        try {
+            //Get the relevant data
+            String newSID = sidInput.getText();
+            float newAssignments = Float.parseFloat(assignmentsInput.getText());
+            float newMidterm = Float.parseFloat(midtermInput.getText());
+            float newExam = Float.parseFloat(examInput.getText());
+
+            //check that the fields were actually populated
+            //Clear the text fields
+            sidInput.clear();
+            assignmentsInput.clear();
+            midtermInput.clear();
+            examInput.clear();
+
+            //Create a new StudentRecord object
+            StudentRecord newStudent = new StudentRecord(newSID, newMidterm, newAssignments, newExam);
+
+            //Add new student to list of students and display the updated list on the table
+            studentList.add(newStudent);
+            table.setItems(studentList);
+        }catch(NumberFormatException e){
+            errorLabel.setText("Grade Error: Inputed grades are not numbers");
+        }
+    }
 }
